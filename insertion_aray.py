97d2a99a8a080c404cec32058aaa83989616d810
hyokyun <hyokyun@disquiet.io>
2022-01-11 15:17:44 +0900

    insertion array

---
+++ insertion_aray.py
@@ -0,0 +1,15 @@
+##selected array
+## 1 2 3 4 5 6 7 8 9 10
+
+array = [1, 10, 5, 8, 7, 6, 4, 3, 2, 9]
+arrayLength = len(array)
+
+for i in range(0, arrayLength-1):
+    j = i
+    while array[j] > array[j+1]:
+        temp = array[j]
+        array[j] = array[j+1]
+        array[j+1] = temp
+        j = j-1
+
+print(array)
