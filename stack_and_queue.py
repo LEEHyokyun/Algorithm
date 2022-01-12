25c1817276ae3c9f606c7afc293e29e170926625
hyokyun <hyokyun@disquiet.io>
2022-01-12 18:44:38 +0900

    stack, queue method

---
+++ stack_and_queue.py
@@ -0,0 +1,17 @@
+stack = [1,2,3,4]
+
+stack.append(7)
+print(stack)
+stack.pop()
+print(stack)
+
+print(stack)
+
+queue = [1,2,3,4]
+
+queue.append(7)
+print(queue)
+queue.pop(0)
+print(queue)
+queue.insert(0,7)
+print(queue)
